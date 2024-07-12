378d594733f6f8f9326895e3f90c67508bae51c9
sampsasd <sampsa.ahopelto@gmail.com>
2024-07-12 11:14:09 +0300

    asd

---
+++ currentSweepLaser.py
@@ -0,0 +1,12 @@
+import pyvisa as visa
+from tkinter import Tk
+from tkinter.ttk import *
+from tkinter import simpledialog, filedialog
+from time import sleep
+import threading
+import tkinter as tk
+import numpy as np
+
+class laserGUI:
+    def __init__(self) -> None:
+        pass
