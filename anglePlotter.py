32bd212efde2fdb6a55c2c6e10614b9405c44746
sampsasd <sampsa.ahopelto@gmail.com>
2024-07-28 11:54:07 +0300

    angle plotter

--- anglePlotter.py
+++ anglePlotter.py
@@ -13,11 +13,12 @@ class apGUI:
 
         self.master = master
         self.master.title('Angle Plotter')
-        self.master.attributes('-fullscreen', True)
+        self.master.geometry('300x600')
+        #self.master.attributes('-fullscreen', True)
         self.master.configure(background='black')
 
-        self.angleList = [0, 10, 20, 30, 40, 50, 60, 70, 80]
-        self.currentList = []
+        self.angleList = [-20, -10, 0, 10, 20, 30, 40, 50, 60, 70, 100, 110, 120]
+        self.currentList = [0, 0.001, 0.005, 0.01, 0.015, 0.02, 0.025, 0.03]
         self.measList = []
         self.stdList = []
         self.filesDict = {}
@@ -60,11 +61,7 @@ class apGUI:
 
         #=============================BUTTONS==================================================
 
-        self.multiVar = IntVar(value=0)
-        self.multiCheck = Checkbutton(self.frm, text='Plot multiple', style='my.TCheckbutton', variable=self.multiVar, onvalue=1, offvalue=0)
-        self.multiCheck.grid(column=0, row=2, padx=5, pady=5)
-
-        self.fileBut = Button(self.frm, text='File(s)', style='my.TButton', command=self.readMeas)
+        self.fileBut = Button(self.frm, text='Files', style='my.TButton', command=self.readMeas)
         self.fileBut.grid(column=0, row=3, padx=5, pady=5)
 
         self.clearDataBut = Button(self.frm, text='Clear data', style='my.TButton', command=self.clear)
@@ -86,76 +83,60 @@ class apGUI:
         
 
     def scatter(self):
-        """Scatters  Power / uW as a function of current / mA"""
+        """Scatters  Power / uW as a function of angle from specular / deg"""
 
-        if not self.multiVar.get():
-            try:
-                plt.scatter(self.angleList, self.measList, marker='o', s=10, c='mediumorchid')
-                if self.errorVar.get():
-                    plt.errorbar(self.angleList, self.measList, yerr=self.stdList, fmt='none', capsize=4, c='mediumorchid')
-                plt.xlabel('Angle from specular / deg')
-                plt.ylabel('Power / $\\mathrm{\\mu}$W')
-                plt.show()
-            except Exception as e:
-                print(e)
-        if self.multiVar.get():
-            try:
-                if not self.errorVar.get():
-                    for key in self.filesDict:
-                        plt.scatter(self.filesDict[key][0], self.filesDict[key][1], marker='o', s=10, c=self.mColor)
-                if self.errorVar.get():
-                    for key in self.filesDict:
-                        plt.scatter(self.filesDict[key][0], self.filesDict[key][1], marker='o', s=10, c=self.mColor)
-                        plt.errorbar(self.filesDict[key][0], self.filesDict[key][1], yerr=self.filesDict[key][2], fmt='none', capsize=4, c=self.mColor)
-                plt.xlabel('Angle from specular / deg')
-                plt.ylabel('Power / $\\mathrm{\\mu}$W')
-                plt.show()
-            except Exception as e:
-                print(e)
-
-    
+        self.angleList = []
+        self.measDict = {}
+        self.stdDict = {}
+        for key in self.filesDict:
+            self.angleList.append(key)
+        for i in range(len(self.filesDict[self.angleList[0]][0])):
+            self.measDict[self.filesDict[self.angleList[0]][0][i]] = [self.filesDict[key][1][i] for key in self.filesDict]
+            self.stdDict[self.filesDict[self.angleList[0]][0][i]] = [self.filesDict[key][2][i] for key in self.filesDict]
+        try:
+            if not self.errorVar.get():
+                for key in self.measDict:
+                    plt.scatter(self.angleList, self.measDict[key], color='mediumorchid')
+                    plt.xlabel('Angle from specular / deg')
+                    plt.ylabel('Power / $\\mathrm{\\mu}$W')
+                    plt.show()
+            if self.errorVar.get():
+                for key in self.filesDict:
+                    plt.scatter(self.filesDict[key][0], self.filesDict[key][1], marker='o', s=10, c=self.mColor)
+                    plt.errorbar(self.filesDict[key][0], self.filesDict[key][1], yerr=self.filesDict[key][2], fmt='none', capsize=4, c=self.mColor)
+            plt.xlabel('Angle from specular / deg')
+            plt.ylabel('Power / $\\mathrm{\\mu}$W')
+            #plt.show()
+        except Exception as e:
+            print(e)
 
     def readMeas(self):
         """Reads current in mA, power and 2*std in uW.\n
         Deletes first row of file and assumes ', ' separator"""
         
-        if not self.multiVar.get():
-            fileName = askopenfilename(initialdir='./AppsNshit/Data', filetypes=(('csv files', 'csv'), ))
-            with open(fileName, 'r') as file:
+        fileNameList = askopenfilenames(initialdir='./AppsNshit/Data', filetypes=(('csv files', 'csv'), ))
+        for i in range(len(fileNameList)):
+            currTemp = []
+            measTemp = []
+            stdTemp = []
+            with open(fileNameList[i], 'r') as file:
                 for row in file:
                     points = row.strip().split(', ')
-                    self.currentList.append(points[0])
-                    self.measList.append(points[1])
-                    self.stdList.append(points[2])
-                self.currentList.remove(self.currentList[0])
-                self.measList.remove(self.measList[0])
-                self.stdList.remove(self.stdList[0])
-                if self.currentList[0] != '-':
-                    self.currentList = [float(point)*1e3 for point in self.currentList]
-                self.measList = [float(point)*1e6 for point in self.measList]
-                self.stdList = [2*float(point)*1e6 for point in self.stdList]
-            self.clearDataBut.configure(state='normal')
-            
-        if self.multiVar.get():
-            fileNameList = askopenfilenames(initialdir='./AppsNshit/Data', filetypes=(('csv files', 'csv'), ))
-            for i in range(len(fileNameList)):
-                currTemp = []
-                measTemp = []
-                stdTemp = []
-                with open(fileNameList[i], 'r') as file:
-                    for row in file:
-                        points = row.strip().split(', ')
-                        currTemp.append(points[0])
-                        measTemp.append(points[1])
-                        stdTemp.append(points[2])
-                    currTemp.remove(currTemp[0])
-                    measTemp.remove(measTemp[0])
-                    stdTemp.remove(stdTemp[0])
-                    if currTemp[0] != '-':
-                        self.filesDict[i] = ([float(point)*1e3 for point in currTemp], [float(point)*1e6 for point in measTemp], [2*float(point)*1e6 for point in stdTemp])
-                    else:
-                        self.filesDict[i] = (currTemp, [float(point)*1e6 for point in measTemp], [2*float(point)*1e6 for point in stdTemp])
-            self.clearDataBut.configure(state='normal')
+                    currTemp.append(points[0])
+                    measTemp.append(points[1])
+                    stdTemp.append(points[2])
+                currTemp.remove(currTemp[0])
+                measTemp.remove(measTemp[0])
+                stdTemp.remove(stdTemp[0])
+                nameAngles = [('_0deg', 0), ('_10deg', 10), ('_20deg', 20), ('_30deg', 30), 
+                              ('_40deg', 40), ('_50deg', 50), ('_60deg', 60), ('_70deg', 70), 
+                              ('_100deg', 100), ('_110deg', 110), ('_120deg', 120), ('neg10deg', -10), 
+                              ('neg20deg', -20)]
+                for tup in nameAngles:
+                    if tup[0] in fileNameList[i]:
+                        self.filesDict[tup[1]] = ([float(point)*1e3 for point in currTemp], [float(point)*1e6 for point in measTemp], [2*float(point)*1e6 for point in stdTemp])
+                
+        self.clearDataBut.configure(state='normal')
 
     def clear(self):
         #self.angleList.clear()
