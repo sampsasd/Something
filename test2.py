73fe9fa036866b3d22ed63997d2ea4b25f985db4
sampsasd <sampsa.ahopelto@gmail.com>
2024-10-03 17:35:34 +0300

    qdsdsvagvaerrg

--- test2.py
+++ test2.py
@@ -3,10 +3,10 @@ import numpy as np
 from time import sleep
 
 thiccboi = [0, 4.96, 10.035, 20.18, 29.74, 41.42, 88.06, 155.8]
-fatboi = [1.062, 1.619, 2.138, 2.685, 3.662, 4.138, 4.375, 5.672]
+fatboi = [1.062e-8, 1.619e-8, 2.138e-8, 2.685e-8, 3.662e-8, 4.138e-8, 4.375e-8, 5.672e-8]
 
 plt.scatter(thiccboi, fatboi, c='deepskyblue')
-plt.xlabel('Surface density / ug/cm$^{2}$')
-plt.ylabel("Area of conversion dist")
+plt.xlabel('Surface density / $\mathrm{\mu}$g/cm$^{2}$')
+plt.ylabel("Sum of angle dist / W")
 plt.tight_layout()
 plt.show()
