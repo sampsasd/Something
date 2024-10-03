e7d1d054909f90ade0578765692bc5ef75af743d
sampsasd <sampsa.ahopelto@gmail.com>
2024-10-03 16:34:47 +0300

    asdasd

--- test2.py
+++ test2.py
@@ -2,8 +2,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 from time import sleep
 
-thiccboi = [0, 4.96, 10.035, 20.18, 29.74, 41.42, 88.06]
-fatboi = [1.062, 1.619, 2.138, 2.685, 3.662, 4.138, 4.375]
+thiccboi = [0, 4.96, 10.035, 20.18, 29.74, 41.42, 88.06, 155.8]
+fatboi = [1.062, 1.619, 2.138, 2.685, 3.662, 4.138, 4.375, 5.672]
 
 plt.scatter(thiccboi, fatboi, c='deepskyblue')
 plt.xlabel('Surface density / ug/cm$^{2}$')
